853bfdfd35c49aaaa68bf7516ace54c6fd6dfd3f
Naranovich <nik.naranovich@mail.ru>
2023-03-16 13:06:26 +0300

    ArrayList<Integer> 1

---
+++ src/main/java/org/example/task5.java
@@ -0,0 +1,23 @@
+package org.example;
+
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class task5 {
+
+    public static void main(String[] args) {
+        List <Integer> listInt = Arrays.asList(5,7,8,3,2,5,10,12,16);
+        List <Integer> result = listInt.stream().reduce(0, (a, b) -> a + b)%2 == 0 ?
+                filter(listInt, p->p%2==0) : filter(listInt, p->p%2!=0);
+        System.out.println(result);
+    }
+
+    private static List <Integer> filter(List <Integer> listInt, Predicate <Integer> predicate) {
+        return listInt.stream().filter(predicate).collect(Collectors.toList());
+    }
+
+}
