d511a04b4a2867506e988d85d9d08b977029dea5
Naranovich <nik.naranovich@mail.ru>
2023-04-05 23:55:51 +0300

    lesson2

---
+++ src/main/java/lesson2/Finder.java
@@ -0,0 +1,10 @@
+package lesson2;
+
+import java.util.List;
+
+public interface Finder {
+    public List getParents();
+    public List getChildren();
+    public List getSubParents();
+    public List getSubChildren();
+}
---
+++ src/main/java/lesson2/TreeNode.java
@@ -0,0 +1,30 @@
+package lesson2;
+
+public abstract class TreeNode {
+    private String name;
+    private String surname;
+
+    private TreeNode parent;
+
+    private TreeNode child;
+
+    public void getParent(){}
+
+    public void setParent(){}
+
+    public void getChild(){}
+
+    public void setChild(){}
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+
+    public String getNameData(){
+        return "Person is " + name + " " + surname;
+    }
+}
